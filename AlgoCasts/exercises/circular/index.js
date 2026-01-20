b8c6ca497ab9ec001524512b942df659a6ec741d
Shaurya <shaurya26@gmail.com>
2026-01-20 14:53:20 +0530

    circu 20 jan.

--- AlgoCasts/exercises/circular/index.js
+++ AlgoCasts/exercises/circular/index.js
@@ -12,6 +12,25 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+
+	let head = list.head;
+	let fast = list.head;
+	let slow = list.head;
+
+	// console.log(head.next)
+	if(head.next == head) return true;
+
+	while(fast.next != null && fast.next.next != null){
+		console.log(fast, slow, "fast", "slow")
+		if(slow == fast.next || slow == fast.next.next) {
+			return true
+		}
+		slow = slow.next;
+		fast = fast.next.next;
+	}
+
+	return false
+}
 
 module.exports = circular;
