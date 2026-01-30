1becb8c5d9f3cd65fe8e087da1a45218e7a88130
Shaurya <shaurya26@gmail.com>
2026-01-30 14:52:05 +0530

    level width. 30jan.

--- AlgoCasts/exercises/levelwidth/index.js
+++ AlgoCasts/exercises/levelwidth/index.js
@@ -12,14 +12,37 @@
 // Answer: [1, 3, 2]
 
 function levelWidth(root) {
-	let arrayCounter = [0];
+	let arrayCounter = [];
+	let idx = 0
 	let baseArray = [];
 
-	baseArray.push(root.head);
+	arrayCounter.push(0);
+	baseArray.push(root);
+	baseArray.push('S');
+
 
 	while(baseArray.length != 0){
-		
+		// console.log("iter")
+		let curr = baseArray.shift();
+		if(curr == 'S') {
+			idx++;
+			if (baseArray.length > 0) {
+		        arrayCounter.push(0);
+		        baseArray.push('S');
+		    }
+		} else {
+				if(curr.children.length == 0) {
+					arrayCounter[idx] = arrayCounter[idx] + 1;
+				} else if(curr.children.length > 0) {
+					//need S before the new children are pushed
+					baseArray.push(...curr.children)
+					arrayCounter[idx] = arrayCounter[idx] + 1;
+				}
+			}
 	}
+	console.log(arrayCounter)
+	console.log('========================')
+	return arrayCounter
 
 }
 
